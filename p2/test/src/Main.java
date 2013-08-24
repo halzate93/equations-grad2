fadcfb57c09c6fe45f5cc5565f7986fe423823cf
halzate93 <halzate93@halzate93-Inspiron-N5110.(none)>
2013-08-23 21:25:42 -0500

    P2 pseudocode done

---
+++ p2/test/src/Main.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author halzate93
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
